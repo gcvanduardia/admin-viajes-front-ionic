a837a9906633f4b3567db82e9268c77ab99da379
David Suarez <davidsuarez@MacBook-Pro-de-David.local>
2024-06-01 17:50:45 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'viajes-admin-front-ionic',
+  webDir: 'www'
+};
+
+export default config;
